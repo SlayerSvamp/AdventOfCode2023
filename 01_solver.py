041bb18bf89f4fe18a13512e47c343d874773807
Linus Aspelin <linus.aspelin@gmail.com>
2023-12-01 14:35:25 +0100

    day 1 done!

---
+++ 01_solver.py
@@ -0,0 +1,33 @@
+# Day 1: Trebuchet?!
+
+import re
+
+
+name = 'Day 1: Trebuchet?!'
+
+part_one_verified = 55834
+part_two_verified = 53221
+
+
+def part_one(lines: list[str]):
+    total = 0
+    for line in lines:
+        nums = [*filter(str.isnumeric, line)]
+        total += int(nums[0] + nums[-1])
+    return total
+
+
+def part_two(lines: list[str]):
+    numbers = ['zero', 'one', 'two', 'three', 'four',
+               'five', 'six', 'seven', 'eight', 'nine']
+    total = 0
+    for line in lines:
+        number_indices = [
+            (i, str(numbers.index(n)))
+            for i in range(len(line))
+            for n in numbers if line[i:].startswith(n)
+        ]
+        digits = [(i, c) for i, c in enumerate(line) if c.isnumeric()]
+        nums = [x[1] for x in sorted(number_indices + digits)]
+        total += int(nums[0] + nums[-1])
+    return total
---
+++ 01_tests.py
@@ -0,0 +1,22 @@
+# Day 1: Trebuchet?! - Tests
+
+part_one = [
+    {'expected': 142, 'input': [
+        '1abc2',
+        'pqr3stu8vwx',
+        'a1b2c3d4e5f',
+        'treb7uchet',
+    ]},
+]
+
+part_two = [
+    {'expected': 281, 'input': [
+        'two1nine',
+        'eightwothree',
+        'abcone2threexyz',
+        'xtwone3four',
+        '4nineeightseven2',
+        'zoneight234',
+        '7pqrstsixteen',
+    ]},
+]
