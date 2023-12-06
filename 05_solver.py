e2680787f4b02913178474199cd644978ebf8ee0
Linus Aspelin <linus.aspelin@gmail.com>
2023-12-06 01:04:25 +0100

    day 5 done!

--- 05_solver.py
+++ 05_solver.py
@@ -3,71 +3,65 @@
 name = 'Day 5: If You Give A Seed A Fertilizer'
 
 part_one_verified = 107430936
-part_two_verified = None
+part_two_verified = 23738616
 
 
-def parse_maps(lines: list[str]):
+def parse_almanac(lines: list[str]):
     seeds = list(map(int, lines[0].split(': ')[1].split(' ')))
-    maps: list[list[tuple[int, int, int]]] = []
+    category_maps: list[list[tuple[int, int, int]]] = []
     for i in range(1, len(lines)):
         line = lines[i]
         if not line:
-            maps.append([])
-        elif line.endswith(':'):
-            continue
-        else:
-            maps[-1].append(tuple(map(int, line.split(' '))))
-    return seeds, maps
-
-
-def get_value(item: int, mapping: list[tuple[int, int, int]]):
-    for destination, source, length in mapping:
-        if item >= source and item <= source + length:
-            return item - source + destination
-    return item
-
-
-def overlap_ranges(start, length, cut_start, cut_length, replace_offset):
-    if start < cut_start:
-        pre_length = max(0, min(cut_start - start, length))
-        yield (start, pre_length)
-        if pre_length < length:
-            length -= pre_length
-            start = cut_start
-        if length < 1:
-            return
-    if start > cut_start and cut_start + cut_length > start:
-        cut_start, cut_length = start, cut_length - (start - cut_start)
-    if start == cut_start:
-        yield (cut_start + replace_offset, min(cut_length, length))
-        length -= cut_length
-        if length < 1:
-            return
-    if start >= cut_start + cut_length:
-        yield (start, length)
-
-
-def get_ranges(item: tuple[int, int], mapping: list[tuple[int, int, int]]):
-    item_start, item_length = item
-    for destination, source, map_length in sorted(mapping):
-        yield from overlap_ranges(item_start, item_length, source, map_length, destination - source)
+            category_maps.append([])
+        elif not line.endswith(':'):
+            destination, source, length = map(int, line.split(' '))
+            source_end = source + length - 1
+            map_diff = destination - source
+            category_maps[-1].append((source, source_end, map_diff))
+    return seeds, category_maps
 
 
 def part_one(lines: list[str]):
-    sources, maps = parse_maps(lines)
-    for _map in maps:
-        sources = [get_value(source, _map) for source in sources]
-    return min(sources)
+    items, category_maps = parse_almanac(lines)
+    for category_map in category_maps:
+        next_items = []
+        for item in items:
+            for source, source_end, map_diff in category_map:
+                if item >= source and item <= source_end:
+                    next_items.append(item + map_diff)
+                    break
+            else:
+                next_items.append(item)
+        items = next_items
+    return min(items)
 
 
 def part_two(lines: list[str]):
-    sources, maps = parse_maps(lines)
-    source_ranges = zip(sources[:-2:2], sources[1::2])
-    for _map in maps:
-        source_ranges = [
-            new_range
-            for source_range in source_ranges
-            for new_range in get_ranges(source_range, _map)
-        ]
+    seeds, category_maps = parse_almanac(lines)
+    source_ranges = [
+        (start, start + length - 1)
+        for start, length in zip(seeds[::2], seeds[1::2])
+    ]
+    for category_map in category_maps:
+        next_ranges = set()
+        for start, end in source_ranges:
+            for source, source_end, map_diff in sorted(category_map):
+                if start < source:
+                    next_ranges.add((start, min(end, source - 1)))
+
+                if end >= source and start <= source_end:
+                    next_ranges.add((
+                        max(start, source) + map_diff,
+                        min(end, source_end) + map_diff
+                    ))
+
+                if end > source_end:
+                    start = max(start, source_end + 1)
+                else:
+                    break
+            else:
+                next_ranges.add((start, end))
+
+        source_ranges = next_ranges
 
     return min(start for start, _ in source_ranges)
