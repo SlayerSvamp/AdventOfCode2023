7d82896c402c181a7217c8626cde2bd941d15923
Linus Aspelin <linus.aspelin@gmail.com>
2023-12-07 18:20:21 +0100

    day 7 done!

--- 07_solver.py
+++ 07_solver.py
@@ -6,46 +6,45 @@ import re
 name = 'Day 7: Camel Cards'
 
 part_one_verified = 250232501
-part_two_verified = None
+part_two_verified = 249138943
 
-values = '0123456789TJQKA'
-
-
-def get_rank_base(cards):
-    scards = ''.join(sorted(cards, key=values.index))
-    # five of a kind
-    if re.search(r'(.)\1{4}', scards):
-        return 5
-    # four of a kind
-    if re.search(r'(.)\1{3}', scards):
-        return 4
-    # full house
-    if re.search(r'(.)\1\1(.)\2', scards) or re.search(r'(.)\1(.)\2\2', scards):
-        return 3.5
-    # three of a kind
-    if re.search(r'(.)\1\1', scards):
-        return 3
-    # two pairs
-    if re.search(r'(.)\1.?(.)\2', scards):
-        return 2
-    # one pair
-    if re.search(r'(.)\1', scards):
-        return 1
-    # high card
-    return 0
+values = 'j123456789TJQKA'
 
 
-def parse_hand(line):
+def get_hand_type(cards):
+    cards = ''.join(sorted(cards, key=values.index))
+    expression_types = [
+        (r'(.)\1{4}', 5),
+        (r'(.)\1{3}', 4),
+        (r'(.)\1\1(.)\2|(.)\3(.)\4\4', 3.5),
+        (r'(.)\1\1', 3),
+        (r'(.)\1.?(.)\2', 2.5),
+        (r'(.)\1', 2),
+    ]
+    for exp, hand_type in expression_types:
+        if re.search(exp, cards):
+            return hand_type
+    return 1
+
+
+def parse_hand(line: str):
     cards, bid = line.split(' ')
-    utslag = list(map(values.index, cards))
-    return (get_rank_base(cards), utslag), int(bid)
+    hand_type = (
+        get_hand_type(c for c in cards if c != 'j')
+        + min(4, cards.count('j'))
+    )
+    tiebreaker = list(map(values.index, cards))
+    return (hand_type, tiebreaker), int(bid)
 
 
 def part_one(lines: list[str]):
-    hands = sorted(parse_hand(line) for line in lines)
+    hands = sorted(parse_hand(x) for x in lines)
     ranked = [(i+1, bid) for i, (_, bid) in enumerate(hands)]
-    return sum(rank*bid for rank, bid in ranked)
+    return sum(rank * bid for rank, bid in ranked)
 
 
 def part_two(lines: list[str]):
-    pass
+    return part_one([
+        line.replace('J', 'j')
+        for line in lines
+    ])
--- 07_tests.py
+++ 07_tests.py
@@ -1,15 +1,17 @@
 # Day 7: Camel Cards - Tests
 
+test_input = [
+    '32T3K 765',
+    'T55J5 684',
+    'KK677 28',
+    'KTJJT 220',
+    'QQQJA 483',
+]
+
 part_one = [
-    {'expected': 6440, 'input': [
-        '32T3K 765',
-        'T55J5 684',
-        'KK677 28',
-        'KTJJT 220',
-        'QQQJA 483',
-    ]},
+    {'expected': 6440, 'input': test_input},
 ]
 
 part_two = [
-    {'expected': '', 'input': ['']},
+    {'expected': 5905, 'input': test_input},
 ]
