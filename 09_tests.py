18ed3e40686b76c557533e5db1dfb4cbb24b549c
Linus Aspelin <linus.aspelin@gmail.com>
2023-12-13 00:33:13 +0100

    day 12 done!

--- 09_tests.py
+++ 09_tests.py
@@ -1,11 +1,9 @@
 # Day 9: Mirage Maintenance - Tests
 
 part_one = [
-    {'expected': 114, 'input': [
-        '0 3 6 9 12 15',
-        '1 3 6 10 15 21',
-        '10 13 16 21 30 45',
-    ]},
+    {'expected': 18, 'input': ['0 3 6 9 12 15']},
+    {'expected': 28, 'input': ['1 3 6 10 15 21']},
+    {'expected': 68, 'input': ['10 13 16 21 30 45']},
 ]
 
 part_two = [
--- 12_solver.py
+++ 12_solver.py
@@ -1,45 +1,69 @@
 # Day 12: Hot Springs
 
-from itertools import zip_longest
-
-
 name = 'Day 12: Hot Springs'
 
 part_one_verified = 7163
-part_two_verified = None
+part_two_verified = 17788038834112
 
 
-def arrangements(nono: str, numbers: list[int]):
-    if '?' not in nono:
-        splitted = list(filter(bool, map(len, nono.split('.'))))
-        yield all(x == y for x, y in zip_longest(numbers, splitted))
-    else:
-        for s in '#.':
-            x = nono.replace('?', s, 1)
-            yield from arrangements(x, numbers)
+def parse_record(line: str, unfold: bool):
+    conditions, groups = line.split(' ')
+    groups = [int(n) for n in groups.split(',')]
+    if unfold:
+        groups *= 5
+        conditions = '?'.join([conditions] * 5)
+    return conditions, groups
+
+
+def count_all_arrangements(line, unfold):
+    conditions, groups = parse_record(line, unfold)
+    damaged_count = sum(groups)
+    operational_count = len(conditions)
+    splitting_operational = len(groups) - 1
+    extra_operational = operational_count - damaged_count - splitting_operational
+    seen_endings = {}
+
+    def condition_matches(arrangement):
+        return all(
+            expected == actual
+            for expected, actual in zip(conditions, arrangement)
+            if expected != '?'
+        )
+
+    def count_record_arrangements(remaining_groups, extra_operational, accumulated=''):
+        key = tuple(remaining_groups), extra_operational, len(accumulated)
+        if key in seen_endings:
+            return seen_endings[key]
+        if not condition_matches(accumulated):
+            return 0
+        if not remaining_groups:
+            return condition_matches(accumulated + '.'*extra_operational)
+
+        total = 0
+        [group, *remaining_groups] = remaining_groups
+        for extra in range(extra_operational + 1):
+            operational = '.'*extra
+            damaged = '#'*group
+            total += count_record_arrangements(
+                remaining_groups,
+                extra_operational - extra,
+                accumulated + operational + damaged + '.'
+            )
+        seen_endings[key] = total
+        return total
+
+    return count_record_arrangements(groups, extra_operational)
 
 
 def part_one(lines: list[str]):
-
-    # [expected in ('?', actual) for expected,actual in zip(nono, alt)]
-
-    total_arrangements = 0
-    for line in lines:
-        nono, numbers = line.split(' ')
-        numbers = list(map(int, numbers.split(',')))
-        total_arrangements += sum(arrangements(nono, numbers))
-
-    return total_arrangements
+    return sum(
+        count_all_arrangements(line, False)
+        for line in lines
+    )
 
 
 def part_two(lines: list[str]):
-    return False
-    total_arrangements = 0
-    for line in lines:
-        nono, numbers = line.split(' ')
-        numbers = list(map(int, numbers.split(',')))
-        nono = nono*5
-        numbers = numbers*5
-        total_arrangements += sum(arrangements(nono, numbers))
-
-    return total_arrangements
+    return sum(
+        count_all_arrangements(line, True)
+        for line in lines
+    )
