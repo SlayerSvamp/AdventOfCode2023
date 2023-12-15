84ace1e1ded25e54927c41cd46b5dc2e425a2032
Linus Aspelin <linus.aspelin@gmail.com>
2023-12-15 01:41:31 +0100

    day 14 done and better!

--- 14_solver.py
+++ 14_solver.py
@@ -3,72 +3,68 @@
 name = 'Day 14: Parabolic Reflector Dish'
 
 part_one_verified = 106517
-part_two_verified = None
+part_two_verified = 79723
 
 
-def parse_field(lines):
-    square_stones = set()
-    rolling_stones = set()
+def parse_platform(lines):
+    north = len(lines) + 1
+    east = len(lines[0]) + 1
+    north_edge = set((x, north) for x in range(east))
+    south_edge = set((x, 0) for x in range(east))
+    west_edge = set((0, y) for y in range(north))
+    east_edge = set((east, y) for y in range(north))
+    rounded_rocks = set()
+    cube_shaped_rocks = set()
     for i, line in enumerate(lines):
         y = len(lines) - i
         for j, c in enumerate(line):
             x = j + 1
             if c == '#':
-                square_stones.add((x, y))
+                cube_shaped_rocks.add((x, y))
             if c == 'O':
-                rolling_stones.add((x, y))
-    bounds = len(lines[0]), len(lines)
-    return square_stones, rolling_stones, bounds
+                rounded_rocks.add((x, y))
+    obstacles = cube_shaped_rocks
+    obstacles |= north_edge | south_edge | west_edge | east_edge
+    return obstacles, rounded_rocks
 
 
-def move_stones(square_stones, rolling_stones, bounds, direction):
-    bx, by = bounds
-    while True:
-        moved = False
-        for old in list(rolling_stones):
-            new = tuple(map(sum, zip(old, direction)))
-            nx, ny = new
-            if new not in square_stones \
-                and new not in rolling_stones \
-                    and ny <= by and ny > 0 and nx <= bx and nx > 0:
-                rolling_stones.remove(old)
-                rolling_stones.add(new)
-                moved = True
-        if not moved:
-            break
+def tilt_platform(obstacles, rounded_rocks, direction):
+    dx, dy = direction
+    any_rock_rolled = True
+    while any_rock_rolled:
+        any_rock_rolled = False
+        for x, y in list(rounded_rocks):
+            rock_rolled = False
+            rolled = (rx, ry) = (x+dx, y+dy)
+            while rolled not in obstacles and rolled not in rounded_rocks:
+                rolled = (rx, ry) = (rx+dx, ry+dy)
+                rock_rolled = True
+            if rock_rolled:
+                rounded_rocks.remove((x, y))
+                rounded_rocks.add((rx-dx, ry-dy))
+                any_rock_rolled = True
 
 
 def part_one(lines: list[str]):
-    square_stones, rolling_stones, bounds = parse_field(lines)
-    move_stones(square_stones, rolling_stones, bounds, (0, 1))
-    return sum(y for _, y in rolling_stones)
+    obstacles, rounded_rocks = parse_platform(lines)
+    tilt_platform(obstacles, rounded_rocks, (0, 1))
+    return sum(y for _, y in rounded_rocks)
 
 
 def part_two(lines: list[str]):
-    goal_cycles = 1000_000_000
-    square_stones, rolling_stones, bounds = parse_field(lines)
-    seen = set()
-    seen_list = []
-    current = tuple(sorted(rolling_stones))
-    seen.add(current)
-    seen_list.append(current)
-    while True:
+    obstacles, rounded_rocks = parse_platform(lines)
+    rock_history = []
+    current = tuple(sorted(rounded_rocks))
+    while current not in rock_history:
+        rock_history.append(current)
         for direction in [(0, 1), (-1, 0), (0, -1), (1, 0)]:
-            move_stones(square_stones, rolling_stones, bounds, direction)
-        current = tuple(sorted(rolling_stones))
-        if current in seen:
-            break
-        seen.add(current)
-        seen_list.append(current)
+            tilt_platform(obstacles, rounded_rocks, direction)
+        current = tuple(sorted(rounded_rocks))
 
-    loop_start = seen_list.index(current)
-    loop_length = len(seen_list) - loop_start
-    print()
-    print(loop_start)
-    print(loop_length)
-    cycles_left = goal_cycles - len(seen_list)
+    loop_start = rock_history.index(current)
+    loop_length = len(rock_history) - loop_start
+    cycles_left = 1000_000_000 - len(rock_history)
     cycles_left %= loop_length
 
-    final_stones = seen_list[loop_start + cycles_left]
-    # 79714 too low
-    return sum(y for _, y in final_stones)
+    final_rocks = rock_history[loop_start + cycles_left]
+    return sum(y for _, y in final_rocks)
