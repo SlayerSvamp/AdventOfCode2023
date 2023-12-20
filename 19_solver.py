43af9d9a19d9a640b7f16b02c372bc7ba1dceb8b
Linus Aspelin <linus.aspelin@gmail.com>
2023-12-20 01:22:11 +0100

    day 19 done!

--- 19_solver.py
+++ 19_solver.py
@@ -1,59 +1,89 @@
 # Day 19: Aplenty
 
-import re
-
-
 name = 'Day 19: Aplenty'
 
 part_one_verified = 434147
-part_two_verified = None
+part_two_verified = 136146366355609
 
 
 def parse_workflows(lines):
-    workflows = {}
-    for line in lines:
-        name, rules = line[:-1].split('{')
-        workflows[name] = rules.split(',')
-    return workflows
+    def pairs():
+        end = lines.index('')
+        for line in lines[:end]:
+            name, rules = line[:-1].split('{')
+            yield name, rules.split(',')
+    return dict(pairs())
 
 
 def parse_parts(lines):
-    return [
-        {
-            k: int(v) for k, v
-            in [raw.split('=') for raw in line[1:-1].split(',')]}
-        for line in lines
-    ]
+    start = lines.index('') + 1
+    for line in lines[start:]:
+        part = {}
+        for pair in line[1:-1].split(','):
+            category, rating = pair.split('=')
+            part[category] = int(rating)
+        yield part
 
 
-def process_part(workflows, part):
-    workflow = 'in'
-    while workflow in workflows:
-        for rule in workflows[workflow]:
-            if rule in 'RA':
-                return rule
-            if rule in workflows:
-                workflow = rule
-                break
-            if '<' in rule:
-                key, value, next_workflow = re.split(r'[<:]', rule)
-                if part[key] < int(value):
-                    workflow = next_workflow
-                    break
-            elif '>' in rule:
-                key, value, next_workflow = re.split(r'[>:]', rule)
-                if part[key] > int(value):
-                    workflow = next_workflow
-                    break
-    return workflow
+def parse_rule(rule):
+    [category, _, *rating], key = rule.split(':')
+    return key, category, int(''.join(rating))
+
+
+def part_is_accepted(workflows, part):
+    def test_key(key):
+        if key in 'RA':
+            return key == 'A'
+        for rule in workflows[key]:
+            if rule in 'RA' or rule in workflows:
+                return test_key(rule)
+            next_key, category, rating = parse_rule(rule)
+            if '<' in rule and part[category] < rating:
+                return test_key(next_key)
+            if '>' in rule and part[category] > rating:
+                return test_key(next_key)
+    return test_key('in')
+
+
+def count_accepted_combinations(workflows):
+    def inner(ratings, key):
+        if key == 'R':
+            return 0
+        if key == 'A':
+            combinations = 1
+            for min_rating, max_rating in ratings.values():
+                combinations *= max_rating - min_rating + 1
+            return combinations
+        acceptable_combinations = 0
+        for rule in workflows[key]:
+            next_ratings = dict(ratings)
+            next_key = rule
+            if ':' in rule:
+                next_key, category, rating = parse_rule(rule)
+                min_rating, max_rating = ratings[category]
+                if '<' in rule:
+                    next_ratings[category] = (min_rating, rating - 1)
+                    ratings[category] = (rating, max_rating)
+                elif '>' in rule:
+                    next_ratings[category] = (rating + 1, max_rating)
+                    ratings[category] = (min_rating, rating)
+            acceptable_combinations += inner(next_ratings, next_key)
+        return acceptable_combinations
+
+    all_ratings = {category: (1, 4000) for category in 'xmas'}
+    return inner(all_ratings, 'in')
 
 
 def part_one(lines: list[str]):
-    i = lines.index('')
-    workflows = parse_workflows(lines[:i])
-    parts = parse_parts(lines[i+1:])
-    return sum(sum(part.values()) for part in parts if process_part(workflows, part) == 'A')
+    workflows = parse_workflows(lines)
+    parts = parse_parts(lines)
+    return sum(
+        sum(part.values())
+        for part in parts
+        if part_is_accepted(workflows, part)
+    )
 
 
 def part_two(lines: list[str]):
-    pass
+    workflows = parse_workflows(lines)
+    return count_accepted_combinations(workflows)
--- 19_tests.py
+++ 19_tests.py
@@ -1,27 +1,29 @@
 # Day 19: Aplenty - Tests
 
+test_input = [
+    'px{a<2006:qkq,m>2090:A,rfg}',
+    'pv{a>1716:R,A}',
+    'lnx{m>1548:A,A}',
+    'rfg{s<537:gd,x>2440:R,A}',
+    'qs{s>3448:A,lnx}',
+    'qkq{x<1416:A,crn}',
+    'crn{x>2662:A,R}',
+    'in{s<1351:px,qqz}',
+    'qqz{s>2770:qs,m<1801:hdj,R}',
+    'gd{a>3333:R,R}',
+    'hdj{m>838:A,pv}',
+    '',
+    '{x=787,m=2655,a=1222,s=2876}',
+    '{x=1679,m=44,a=2067,s=496}',
+    '{x=2036,m=264,a=79,s=2244}',
+    '{x=2461,m=1339,a=466,s=291}',
+    '{x=2127,m=1623,a=2188,s=1013}',
+]
+
 part_one = [
-    {'expected': 19114, 'input': [
-        'px{a<2006:qkq,m>2090:A,rfg}',
-        'pv{a>1716:R,A}',
-        'lnx{m>1548:A,A}',
-        'rfg{s<537:gd,x>2440:R,A}',
-        'qs{s>3448:A,lnx}',
-        'qkq{x<1416:A,crn}',
-        'crn{x>2662:A,R}',
-        'in{s<1351:px,qqz}',
-        'qqz{s>2770:qs,m<1801:hdj,R}',
-        'gd{a>3333:R,R}',
-        'hdj{m>838:A,pv}',
-        '',
-        '{x=787,m=2655,a=1222,s=2876}',
-        '{x=1679,m=44,a=2067,s=496}',
-        '{x=2036,m=264,a=79,s=2244}',
-        '{x=2461,m=1339,a=466,s=291}',
-        '{x=2127,m=1623,a=2188,s=1013}',
-    ]},
+    {'expected': 19114, 'input': test_input},
 ]
 
 part_two = [
-    {'expected': '', 'input': ['']},
+    {'expected': 167409079868000, 'input': test_input},
 ]
