9f649634f1c6e5bc245db2714de3ea8bb444c7f7
Linus Aspelin <linus.aspelin@gmail.com>
2023-12-22 16:48:02 +0100

    created files for day 22

---
+++ 22_solver.py
@@ -0,0 +1,22 @@
+# Day 22: Sand Slabs
+
+name = 'Day 22: Sand Slabs'
+
+part_one_verified = None
+part_two_verified = None
+
+
+def part_one(lines: list[str]):
+    for line in lines:
+        c1, c2 = line.split('~')
+        x1, y1, z1 = map(int, c1.split(','))
+        x2, y2, z2 = map(int, c2.split(','))
+    any_falling = True
+    while any_falling:
+        any_falling = False
+        
+
+
+
+def part_two(lines: list[str]):
+    pass
